3e88d618c1c4a3a8d4c193253e549cb9cd181430
Sy Le <lenguyensy@gmail.com>
2022-10-11 13:05:39 -0700

    wip handling oauth

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # google-keep-note-backup
 
 
+https://developers.google.com/drive/api/quickstart/nodejs
 https://developers.google.com/keep/api/reference/rest
 https://github.com/synle/rest-utils
+
+## Start
+
+Download JSON from google cloud API, move it under `credentials.json`
+
+```
+npm start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,90 @@
-import Calc from 'src/Calc';
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
 
-export function doWork() {
-  console.log('======= Named Module Export');
-  console.log(Calc.sum(1, 2));
-  console.log(Calc.sum(2, 3));
-  console.log(Calc.sum(6, 4));
-  console.log(Calc.sum(3, 5));
-  console.log('============================');
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
 }
 
-export default function doDefaultWork() {
-  console.log('======= Default Export');
-  console.log(Calc.sum(1, 2));
-  console.log(Calc.sum(2, 3));
-  console.log(Calc.sum(6, 4));
-  console.log(Calc.sum(3, 5));
-  console.log('============================');
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    refresh_token: process.env.GOOGLE_CLIENT_REFRESH_TOKEN,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
 }
 
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
 
-doWork();
-doDefaultWork();
+/**
+ * Lists the names and IDs of up to 10 files.
+ * @param {OAuth2Client} authClient An authorized OAuth2 client.
+ */
+async function listFiles(authClient) {
+  const drive = google.drive({version: 'v3', auth: authClient});
+  const res = await drive.files.list({
+    pageSize: 10,
+    fields: 'nextPageToken, files(id, name)',
+  });
+  const files = res.data.files;
+  if (files.length === 0) {
+    console.log('No files found.');
+    return;
+  }
+
+  console.log('Files:');
+  files.map((file) => {
+    console.log(`${file.name} (${file.id})`);
+  });
+}
+
+authorize().then(listFiles).catch(console.error);
